be64ca318fd5fb96e71a227931f005d8a831e1f4
organism <coolrabbit1993@gmail.com>
2024-01-24 11:13:02 +0800

    feat: 添加测试类

--- maven-common/src/test/java/com/zero/maven/common/MavenCommonApplicationTest.java
+++ maven-common/src/test/java/com/zero/maven/common/MavenCommonApplicationTest.java
@@ -1,10 +1,17 @@
 package com.zero.maven.common;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author rabbit
  * @description 测试类
  * @createTime 2024/1/24 10:20
  */
 public class MavenCommonApplicationTest {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello world");
+    }
 
 }
